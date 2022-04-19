4c3ffcbb447a7f2c4c7f295ed97f88a1997fe806
Jehu Frayle <jehufrayle@gmail.com>
2022-04-19 10:23:35 -0700

    Added a remove button to each image

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import { registerImage } from '../utils/lazy.js';
 const app = document.getElementById("images");
 const genButton = document.getElementById('genButton');
 const cleanButton = document.getElementById('cleanButton');
-const remButton = document.getElementById('remButton');
 
 let manyFoxes = 0;
 genButton.addEventListener('click', generateFox);
 cleanButton.addEventListener('click', cleanScreen);
-remButton.addEventListener('click', removeOne);
 
 
 function generateFox() {
@@ -16,19 +14,27 @@ function generateFox() {
     const fox = document.createElement('img');
     fox.dataset.src = `https://randomfox.ca/images/${randomNumber(1,123)}.jpg`;
     fox.alt = `Fox ${manyFoxes}`;
-    container.className = `fox`;
+    container.className = `fox fox${manyFoxes}`;
     container.appendChild(fox);
+
+    const remButton = document.createElement('button');
+    remButton.type = 'button';
+    remButton.className = 'remButton';
+    remButton.addEventListener('click', removeOne);
+    container.appendChild(remButton);
+
     app.appendChild(container);
     registerImage(container);
     manyFoxes ++;
+    
 }
 function cleanScreen(){
     app.innerHTML = '';
     manyFoxes = 0;
 }
-function removeOne(){
-    app.removeChild(app.lastChild);
-    manyFoxes -= 1;
+function removeOne(event){
+    const toRemove = event.target.parentNode;
+    app.removeChild(toRemove);
 }
 function randomNumber(min, max) {
     min = Math.ceil(min);
