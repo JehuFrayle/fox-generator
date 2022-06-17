ed49db753ab85d86f9e7e44a24818a0b2b2320ee
Jehu Frayle <jehufrayle@gmail.com>
2022-06-16 22:35:04 -0700

    Added a favorite page, but there's no favorite button yet hehe.

---
+++ src/favorites.js
@@ -0,0 +1,43 @@
+import { registerImage } from '../utils/lazy.js';
+const app = document.getElementById("images");
+
+let favorites = JSON.parse(localStorage.getItem("favorites"));
+
+if (favorites.length > 0) {
+    favorites.forEach((favorite, ind) => {
+        generateFox(favorite, ind)
+    })
+} else {
+    const message = document.createElement('h2');
+    message.className = '.noFavoritesMessage';
+    message.innerHTML = "You don't have favorites yet!"
+
+    app.appendChild(message);
+}
+function generateFox(url, index) {
+    const container = document.createElement('div');
+
+    const fox = document.createElement('img');
+    fox.dataset.src = `https://randomfox.ca/images/${url}.jpg`;
+    fox.alt = `Favorite fox number: ${index}`;
+    container.dataset.id = url;
+    container.className = `fox fox${index}`;
+    container.appendChild(fox);
+
+    const remButton = document.createElement('button');
+    remButton.type = 'button';
+    remButton.className = 'remButton';
+    remButton.addEventListener('click', removeFromFavorites);
+    container.appendChild(remButton);
+
+    app.appendChild(container);
+    registerImage(container);
+}
+function removeFromFavorites(event) {
+    const toRemove = event.target.parentNode;
+    const memoryId = Number(event.target.parentNode.dataset.id);
+
+    favorites.splice(favorites.findIndex((item) => item === memoryId), 1);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    app.removeChild(toRemove);
+}
